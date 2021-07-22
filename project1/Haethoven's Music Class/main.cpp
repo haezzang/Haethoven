72c9c9408eecf8ca55b43017e16868502e4f9176
[haezzang] <songna89@naver.com>
2021-07-22 18:17:44 +0900

    메뉴 이동 구현

--- project1/Haethoven's Music Class/main.cpp
+++ project1/Haethoven's Music Class/main.cpp
@@ -6,16 +6,19 @@
 using namespace std;
 //디코딩 폰트 32
 
-//방향 정의
-#define UP = 0
-#define DOWN = 1
-#define RIGHT = 2
-#define LEFT = 3
+//키보드 정의
+#define UP  0
+#define DOWN  1
+#define RIGHT  3
+#define LEFT  4
+#define SPACE 5
+#define ESC 6
 
 
-//함수프로토
+//함수 정의
 void ReadyGame();
 void StartGame();
+int keyControl();
 
 
 //좌표함수
@@ -28,11 +31,34 @@ void gotoxy(int x, int y)
 }
 
 
-//준비화면
-void ReadyGame() {
+//커서 제어
+int keyControl() {
+    char c;
+    while (1) {
+        if (_kbhit()) {               
+            c = _getch();          
+            if (c == -32) {         
+                c = _getch();        
+                switch (c) {
+                case 72:
+                    return UP; break;
+                case 80:
+                    return DOWN;  break;
+                case 75:
+                    return LEFT; break;
+                case 77:
+                    return RIGHT; break;
+                }
+            }
+        }
+    }
+}
 
+
+//타이틀
+void Title() {
     //아스키아트
-    gotoxy(13,4);
+    gotoxy(13, 4);
     cout << " _   _   ___   _____  _____  _____  _   _  _____  _   _ " << endl;
     gotoxy(13, 5);
     cout << "| | | | / _ \\ |  ___||_   _||  _  || | | ||  ___|| \\ | |" << endl;
@@ -42,7 +68,7 @@ void ReadyGame() {
     cout << "|  _  ||  _  ||  __|   | |  | | | || | | ||  __| | . ` |" << endl;
     gotoxy(13, 8);
     cout << "| | | || | | || |___   | |  \\ \\_/ /\\ \\_/ /| |___ | |\\  |" << endl;
-    gotoxy(13,9);
+    gotoxy(13, 9);
     cout << "\\_| |_/\\_| |_/\\____/   \\_/   \\___/  \\___/ \\____/ \\_| \\_/" << endl;
     gotoxy(32, 10);
     cout << "해토벤의 음악교실" << endl;
@@ -51,21 +77,57 @@ void ReadyGame() {
     gotoxy(13, 14);
     cout << "|                                                     |" << endl;
     gotoxy(13, 15);
-    cout << "|                     게 임 시 작                     |" << endl;
+    cout << "|                                                     |" << endl;
     gotoxy(13, 16);
     cout << "|                                                     |" << endl;
     gotoxy(13, 17);
-    cout << "|                     랭 킹 보 기                     |" << endl;
+    cout << "|                                                     |" << endl;
     gotoxy(13, 18);
     cout << "|                                                     |" << endl;
     gotoxy(13, 19);
-    cout << "|                     게 임 종 료                     |" << endl;
+    cout << "|                                                     |" << endl;
     gotoxy(13, 20);
     cout << "|                                                     |" << endl;
     gotoxy(13, 21);
     cout << "|_____________________________________________________|" << endl;
 
+
+    // system("pause>null");
 }
+
+    int menu() {
+      
+        int x = 24;
+        int y = 15;
+        gotoxy(x-1, y);
+        cout << "> 게 임 시 작" << endl;
+        gotoxy(x, y+2);
+        cout << "랭 킹 보 기" << endl;
+        gotoxy(x, y+4);
+        cout << "게 임 종 료" << endl;
+        while (1) {
+            int n = keyControl();
+            switch (n)
+            {
+            case UP: {
+                if (y > 15) {
+                    gotoxy(x - 1, y); printf("  ");
+                    gotoxy(x - 1, y-=2); printf(">");
+                }
+                break;
+            }
+            case DOWN : {
+                if (y < 19) {
+                    gotoxy(x - 1, y); printf("  ");
+                    gotoxy(x - 1, y+=2); printf(">");
+                }
+                break;
+            }
+              
+            }
+        }
+    }
+
 
 //게임실행화면
 void StartGame() {
@@ -84,7 +146,9 @@ void StartGame() {
         cout << str[rn] << endl;
     }
 }
-    int main(void) {
-        ReadyGame();
+//메인함수
+int main(void) {
+    Title();
+    menu();
 
-    }
+}
